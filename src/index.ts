0aefe900ef35487a75a163cdf47dd5ead2ab7d38
mobbipin <bipin@mobcreatives.com.np>
2025-07-02 13:24:59 +0545

    CORS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import auth from './routes/auth'
 import players from './routes/players'
 import teams from './routes/teams'
@@ -7,6 +8,15 @@ import posts from './routes/posts'
 import admin from './routes/admin'
 
 const app = new Hono()
+
+app.use('*', cors({
+  origin: [
+    'https://esportszone.mobbysc.com',
+    'http://localhost:5173'
+  ],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}))
 
 app.get('/', (c) => c.text('Hello Hono ! I am MOB ESPORTS SERVER'))
 
